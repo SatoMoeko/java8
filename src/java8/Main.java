22306eac4958e8ca96c14fde7cbaebdca9f5c768
SatoMoeko <mopepeko@gmail.com>
2024-01-10 20:40:53 +0900

    8章コード8-16まで

--- src/java8/Main.java
+++ src/java8/Main.java
@@ -9,19 +9,20 @@ public class Main {
 		//2.フィールドに初期値をセット
 		h.name = "ミナト";
 		h.hp = 100;
-		System.out.println("勇者"+h.name+"を生み出しました！");
-		//3.勇者インスタンスのメソッドを呼び出してゆく
-		h.sit(5);
+		
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A'; //区別するための通し番号をつけることsuffix
+		
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+		
+	//冒険の始まり
 		h.slip();
-		h.sit(25);
+		m1.run();
+		m2.run();
 		h.run();
-		
-		
-		// 以下の内容をJavaで記述していく
-		//勇者よ、この仮想世界に生まれよ！
-		//お化けキノコよ、この仮想世界に生まれよ！
-		//勇者よ、戦え！
-		//お化けキノコよ、逃げろ！
 
 	}
 
--- src/java8/Matango.java
+++ src/java8/Matango.java
@@ -3,5 +3,9 @@ package java8;
 public class Matango {
 	int hp;
 	final int LEVEL = 10; //レベルの数字を指定して初期化
+	char suffix;
+	public void run() {
+		System.out.println("お化けキノコ"+this.suffix+"は逃げ出した！");
+	}
 
 }
